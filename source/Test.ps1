3dba1f886b3fe6c4623488b8370864d17a2295e0
Raúl González <rulasg@github.com>
2023-03-16 18:22:13 +0100

    bug: fix double load of TestingHelper

--- source/Test.ps1
+++ source/Test.ps1
@@ -5,12 +5,11 @@ function Import-TestingHelper($Version){
     
     if (-not (import-Module TestingHelper -RequiredVersion $Version -PassThru -ErrorAction SilentlyContinue )) {
         Install-Module -Name TestingHelper -Force -RequiredVersion $Version
+        Import-Module -Name TestingHelper -Force -RequiredVersion $Version
     }
-    
-    Import-Module -Name TestingHelper -Force -RequiredVersion $Version
 }
 
-Import-TestingHelper -Version $TestingHelperRequiredVersion
+Import-TestingHelper -Version $Version
 
 $psd = get-childitem -Path $PSScriptRoot -Filter *.psd1
 
