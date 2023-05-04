26b02e4b34f69a1c1cfd1bdb52620d917dd8943b
rulasg <rulasg@outlook.com>
2023-05-04 13:50:00 +0200

    Test.ps1 and publish.ps1 improvements Fixes #29

--- test.ps1
+++ test.ps1
@@ -1,18 +1,55 @@
 [CmdletBinding()]
 param ()
 
-function Import-TestingHelper($Version){
+function Import-TestingHelper{
+    [CmdletBinding()]
+    param (
+        [Parameter()][string]$Version,
+        [Parameter()][switch]$AllowPrerelease,
+        [Parameter()][switch]$PassThru
+    )
 
-    if (-not (import-Module TestingHelper -RequiredVersion $Version -PassThru -ErrorAction SilentlyContinue )) {
-        Install-Module -Name TestingHelper -Force -RequiredVersion $Version
-        Import-Module -Name TestingHelper -Force -RequiredVersion $Version
+    
+    if ($Version) {
+        $V = $Version.Split('-')
+        $semVer = $V[0]
+        $AllowPrerelease = ($AllowPrerelease -or ($null -ne $V[1]))
+    }
+    
+    $module = Import-Module TestingHelper -PassThru -ErrorAction SilentlyContinue -RequiredVersion:$semVer
+
+    if ($null -eq $module) {
+        $installed = Install-Module -Name TestingHelper -Force -AllowPrerelease:$AllowPrerelease -passThru -RequiredVersion:$Version
+        $module = Import-Module -Name $installed.Name -RequiredVersion ($installed.Version.Split('-')[0]) -Force -PassThru
+    }
+
+    if ($PassThru) {
+        $module
     }
 }
 
-Import-TestingHelper -Version $Version
+# Import-TestingHelper -Version "2.1.1-alpha"
+uninstall-Module -name TestingHelper -AllVersions ; rmo testinghelper*
+Import-TestingHelper -AllowPrerelease # 2.1.1-alpha
 
-$psd = get-childitem -Path $PSScriptRoot -Filter *.psd1
+Import-TestingHelper -Version 2.5 # 2.5
 
-Import-Module -Name $psd.FullName -Force
+uninstall-Module -name TestingHelper -AllVersions ; rmo testinghelper*   
+Import-TestingHelper -Version 1.3 # 1.3
 
-Test-Module -Name $psd.BaseName
+uninstall-Module -name TestingHelper -AllVersions ; rmo testinghelper*   
+Import-TestingHelper  # 2.0
+
+uninstall-Module -name TestingHelper -AllVersions ; rmo testinghelper*   
+Import-TestingHelper -Version "2.1.1-alpha" # 2.1.1-alpha
+Import-TestingHelper  # 2.1.1-alpha
+
+# Run test by PSD1 file
+# Test-ModulelocalPSD1
+
+# Run tests by module name
+# We need to manage the import of the version we want to test
+# If there are different versions of the module installed, we need to import the one we want to test
+# $psd = get-childitem -Path $PSScriptRoot -Filter *.psd1
+# Import-Module -Name $psd.FullName -Force
+# Test-Module -Name $psd.BaseName
