7dd2eff7983e74d659e9320ca0a46dd59ac2a98a
Raúl González <rulasg@outlook.com>
2023-05-24 17:12:17 +0000

    Move tests to public

--- DemoPsModuleTest/DemoPsModuleTest.psm1
+++ DemoPsModuleTest/DemoPsModuleTest.psm1
@@ -30,34 +30,3 @@ Foreach($import in @($Public + $Private))
         Write-Error -Message "Failed to import function $($import.fullname): $_"
     }
 }
-
-function DemoPsModuleTest_Sample(){
-    Assert-IsTrue -Condition $true
-} Export-ModuleMember -Function DemoPsModuleTest_Sample
-
-function DemoPsModuleTest_GetPrivateFunction(){
-
-    # $module = Import-Module -Name $MODULE_PATH -PassThru -Force
-    $module = Get-Module -Name DemoPsModule
-
-    & $module {
-
-        $result = Get-PrivateFunction -Text "Testing"
-        Assert-AreEqual -Expected ("Private function [{0}]" -f "Testing") -Presented $result
-    }
-} Export-ModuleMember -Function DemoPsModuleTest_GetPrivateFunction
-
-function DemoPsModuleTest_GetPublicFunction(){
-
-    $result = Get-PublicFunction -Text "Testing"
-
-    Assert-AreEqual -Expected ("Public function [{0}]" -f "Testing") -Presented $result
-} Export-ModuleMember -Function DemoPsModuleTest_GetPublicFunction
-
-function DemoPsModuleTest_GetPublicFunctionWithPrivateCall(){
-
-    $result = Get-PublicFunctinWithPrivateCall -Text "Testing"
-
-    Assert-AreEqual -Expected ("Public function [{0}]" -f "Private function [Testing]") -Presented $result
-} Export-ModuleMember -Function DemoPsModuleTest_GetPublicFunctionWithPrivateCall
-
