76e802114345928571431f4a37f33580237e2779
rulasg <rulasg@outlook.com>
2023-05-29 00:09:31 +0200

    Improve publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,12 +1,4 @@
-<#
-.SYNOPSIS
-    Publish the Module to PSGallery
-.DESCRIPTION
-    This script will publish the module to the PSGallery
-.NOTES
-    You will need to create a NuGet API Key for the PSGallery at https://www.powershellgallery.com/account/apikeys
-.LINK
-    https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish.ps1
+<# .SYNOPSIS Publish the Module to PSGallery .DESCRIPTION This script will publish the module to the PSGallery .NOTES You will need to create a NuGet API Key for the PSGallery at https://www.powershellgallery.com/account/apikeys .LINK https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish.ps1
 .EXAMPLE
     # Publish the module to the PSGallery without prompting
 
@@ -44,17 +36,12 @@ if ( [string]::IsNullOrWhiteSpace($NuGetApiKey) ) {
 }
 
 # look for psd1 file on the same folder as this script
-$psdPath = Get-ChildItem -Path $PSScriptRoot -Filter *.psd1
+$moduleName  = $PSScriptRoot | Split-Path -leaf
+$psdPath = $PSScriptRoot | Join-Path -ChildPath "$moduleName.psd1"
 
 # check if $psd is set
-if ( $null -eq $psdPath ) {
+if ( -not (Test-Path -Path $psdPath)) {
     Write-Error -Message 'No psd1 file found'
-    return
-}
-
-# check if $psd is a single file
-if ( $psdPath.Count -gt 1 ) {
-    Write-Error -Message 'More than one psd1 file found'
     return
 }
 
@@ -62,7 +49,6 @@ if ( $psdPath.Count -gt 1 ) {
 $psd1 = Import-PowerShellDataFile -Path $psdPath
 $psd1
 $psd1.PrivateData.PSData
-
 
 # Confirm if not forced
 if ($Force -and -not $Confirm){
