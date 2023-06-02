1ed89fb81b43db5bb252208ed225c2fb0d2d7141
rulasg <rulasg@outlook.com>
2023-06-02 12:17:29 +0200

    Add -Force on release.ps1

--- release.ps1
+++ release.ps1
@@ -1,8 +1,14 @@
 [cmdletbinding(SupportsShouldProcess, ConfirmImpact='High')]
 param(
     # Update the module manifest with the version tag (Sample: v10.0.01-alpha)
-    [Parameter(Mandatory=$false)] [string]$VersionTag
+    [Parameter(Mandatory=$false)] [string]$VersionTag,
+    [Parameter(Mandatory=$false)] [switch]$Force
 )
+
+# Confirm if not forced
+if ($Force -and -not $Confirm){
+    $ConfirmPreference = 'None'
+}
 
 if ($PSCmdlet.ShouldProcess($VersionTag, "git tag creation")) {
     git tag -a $VersionTag -m "Release tag" -s ; git push --tags
